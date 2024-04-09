bd2665ad7fa2d547f3c60f1c6d553f77156fd706
Mark <mrkcarlos576@gmail.com>
2024-04-09 14:57:50 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textParagraph = document.getElementById('text');
+    if (textParagraph) {
+      textParagraph.textContent = 'This is really cool!';
+    }
+  });
+  
+
+
+  
